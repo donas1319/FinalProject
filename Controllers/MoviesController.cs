4a7deb2c9ede02a540fbea8ae885b628598d2037
Dona <67574045+donas1319@users.noreply.github.com>
2021-02-24 01:47:40 -0500

    Add controllers and views

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MovieTicketReservation.Data;
+using MovieTicketReservation.Models;
+
+namespace MovieTicketReservation.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Movies
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.movies.Include(m => m.category);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Movies/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movies = await _context.movies
+                .Include(m => m.category)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movies == null)
+            {
+                return NotFound();
+            }
+
+            return View(movies);
+        }
+
+        // GET: Movies/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.categories, "CategoryId", "Name");
+            return View();
+        }
+
+        // POST: Movies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,MovieName,DirectorName,ReleaseDate,Rating,Price,CategoryId")] Movies movies)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(movies);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.categories, "CategoryId", "Name", movies.CategoryId);
+            return View(movies);
+        }
+
+        // GET: Movies/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movies = await _context.movies.FindAsync(id);
+            if (movies == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.categories, "CategoryId", "Name", movies.CategoryId);
+            return View(movies);
+        }
+
+        // POST: Movies/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,MovieName,DirectorName,ReleaseDate,Rating,Price,CategoryId")] Movies movies)
+        {
+            if (id != movies.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(movies);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MoviesExists(movies.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.categories, "CategoryId", "Name", movies.CategoryId);
+            return View(movies);
+        }
+
+        // GET: Movies/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movies = await _context.movies
+                .Include(m => m.category)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movies == null)
+            {
+                return NotFound();
+            }
+
+            return View(movies);
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var movies = await _context.movies.FindAsync(id);
+            _context.movies.Remove(movies);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MoviesExists(int id)
+        {
+            return _context.movies.Any(e => e.Id == id);
+        }
+    }
+}
